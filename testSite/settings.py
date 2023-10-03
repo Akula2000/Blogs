8cb1ea0df85d2646c30df03eab7917107bb50b5b
shvejan <shvejan2011@gmail.com>
2023-10-03 04:19:35 -0400

    req and settings updated

--- testSite/settings.py
+++ testSite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-aqe3^dk)#sl@u6)qof^h!ka%*m4a%1%eg8rq&x9=ja=jdw*ik@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-env.eba-irssukuk.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['http://django-env-2.eba-ddcbgxeg.us-west-2.elasticbeanstalk.com/']
 
 
 # Application definition
@@ -118,6 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
